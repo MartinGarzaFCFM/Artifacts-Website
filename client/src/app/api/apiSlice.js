900521b8e079c9280cdca20ffb3d52fc9d88e6e6
Martin Garza <54215833+MartinGarzaFCFM@users.noreply.github.com>
2024-05-24 00:41:29 -0600

    Update apiSlice.js

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://artifacts-api-kdx3.onrender.com' }),
     tagTypes: ['Artifact', 'User'],
     endpoints: builder => ({})
 })

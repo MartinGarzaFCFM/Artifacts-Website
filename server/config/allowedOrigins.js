c31dc5919b0fe5e0152fb9b511dcd0b3edc9d8b5
Martin Garza <54215833+MartinGarzaFCFM@users.noreply.github.com>
2024-05-23 21:54:52 -0600

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
     'https://www.google.com',
-    'https://artifacts-frontend.onrender.com'
+    'https://artifacts-frontend.onrender.com',
+    'https://172.178.88.167'
 ]
 
 module.exports = allowedOrigins
